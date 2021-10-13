ad7ab2e4559fb3f6f0cfac7063ec20fdde985e2c
David Sturgis <tenth10th@gmail.com>
2021-10-12 21:19:03 -0400

    Progress from 10/11/2021

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,10 +1,60 @@
-def score_poker_hands(h1, h2):
-    """
-    Given two poker hands, represented by strings, return an integer result:
-        0: Draw (hands have equal value)
-        1: Hand 1 Wins
-        2: Hand 2 Wins
-    """
-    # TODO: Figure out how Poker hands are scored?
+import string
+
+def score_poker_hands(hand_1, hand_2):
+    # TODO: Figure out how Poker hands are scored?   
     # FIXME: Implement Poker scoring here, I guess
+    
+    h1_value = parse_hand(hand_1)[0]
+    h2_value = parse_hand(hand_2)[0]
+
+    # Highest Card Wins
+    if h1_value == h2_value:
+        return 0
+
+    if h1_value < h2_value: 
+        return 2
+    
+    return 1
+
+face_cards = {
+    "A": 14,
+    "K": 13,
+    "Q": 12,
+    "J": 11,
+    "T": 10
+}
+
+def parse_hand(hand):
+    if not isinstance(hand, str):
+        raise ValueError("Invalid Hand string")
+    if not hand:
+        raise ValueError("Invalid empty Hand")
+    cards_str = hand.split(' ')
+    card_values = [parse_card(card_str) for card_str in cards_str]
+    card_values.sort(reverse=True)
+    return card_values
+
+def parse_card(card):
+    if len(card) != 2:
+        raise ValueError("Invalid card")
+    rank_str, suit_str = card
+    
+    if suit_str not in ['H', 'D', 'C', 'S']:
+        raise ValueError("Invalid Suit")
+    
+    try:
+        number_value = int(rank_str)
+    except Exception:
+        pass
+    else:
+        if number_value > 9 or number_value < 2:
+            raise ValueError("Invalid number card")
+        return number_value
+    
+    face_value = face_cards.get(rank_str)
+    if face_value is None:
+        raise ValueError("Unknown face card")
+    return face_value
+
+def is_pair():
     pass
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,5 +1,55 @@
-from poker_scoring_api import score_poker_hands
+from poker_scoring_api import (
+    score_poker_hands,
+    parse_card,
+    parse_hand,
+    is_pair,
+)
+import pytest
 
-def test_example_unit_test():
+
+def test_scoring_api():
     # FIXME: Maybe assert something meaningful about score_poker_hands?
-    assert score_poker_hands is not None
+    assert score_poker_hands("2H", "3H") in (0, 1, 2)
+
+def test_simple_scoring():
+    assert score_poker_hands("2H", "3H") == 2
+    assert score_poker_hands("2H", "2D") == 0
+    assert score_poker_hands("3H", "2D") == 1
+    assert score_poker_hands("TH", "9S") == 1
+    assert score_poker_hands("TH", "JS") == 2
+
+def test_parse_card():
+    assert parse_card("TH") == 10
+    assert parse_card("9S") == 9
+    assert parse_card("JS") == 11
+    
+@pytest.mark.parametrize("card", [
+    ("4"),
+    ("XS"),
+    ("11H"),
+    ("QZ"),
+    ("10C"),
+])
+def test_invalid_cards(card):
+    with pytest.raises(ValueError):
+        parse_card(card)
+
+@pytest.mark.parametrize("hand", [
+    (3),
+    (''),
+])
+def test_invalid_hands(hand):
+    with pytest.raises(ValueError):
+        parse_hand(hand)
+
+def test_multi_card_hand():
+    assert score_poker_hands("2H 7S KC", "5D 5H 5C") == 1
+
+def test_parse_hand():
+    assert parse_hand("2H 7S KC") == [13, 7, 2]
+
+def test_pair_hand():
+    assert score_poker_hands("2H 2C", "3H 4C") == 1
+
+def test_is_pair():
+    assert is_pair("2H 2C") is True
