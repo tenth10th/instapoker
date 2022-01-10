8fefd3b4867b7e7ce1b3f6382afc7d88a7291137
David Sturgis <tenth10th@gmail.com>
2022-01-10 13:08:28 -0500

    Refactor Integration Tests
    * Give all Test cases a docstring (describing high level requirement)
    
    * Give all Assertions a custom failure message (providing more specific context)
    
    * Remove pytest.raises in favor of manual exception code (to allow for custom AssertionError messages)
    
    Should these be considered Acceptance Tests, rather than Integration tests? It's an interesting philosophical question...

--- spoiler_alert_keep_out/test_integrations.py
+++ spoiler_alert_keep_out/test_integrations.py
@@ -1,5 +1,17 @@
+from random import expovariate
 import pytest
 from poker_scoring_api import score_poker_hands
+
+
+def hand_error_str(h1, h2, int_result):
+    if int_result == 1:
+        result_verb = "win"
+    elif int_result == 2:
+        result_verb = "lose"
+    else:
+        result_verb = "draw"
+
+    return f"{h1} should {result_verb} against {h2}!"
 
 
 @pytest.mark.level(0)
@@ -8,7 +20,7 @@ def test_poker_scoring_api():
     score_poker_hands accepts two strings, and returns an integer
     """
     result = score_poker_hands("2D", "2H")
-    assert isinstance(result, int)
+    assert isinstance(result, int), "score_poker_hands output must be an integer!"
 
 
 @pytest.mark.level(1)
@@ -20,44 +32,117 @@ def test_poker_scoring_api():
     ("8D", "7D", 1),
 ])
 def test_single_card_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Single-card hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(1)
 @pytest.mark.max_level(3)
 def test_high_single_card_hands_10():
-    assert score_poker_hands("10H", "9S") == 1
+    """
+    Single-card rank 10 hands are evaluated correctly
+    """
+    assert score_poker_hands("10H", "9S") == 1, "10H should win against 9S!"
 
 
 @pytest.mark.level(2)
-def test_invalid_None_inputs():
-    with pytest.raises(ValueError):
+@pytest.mark.parametrize("h1, h2", [
+    (None, None),
+    ("1S", None),
+    (None, "1S"),
+])
+def test_invalid_None_inputs(h1, h2):
+    """
+    None inputs raise ValueErrors
+    """
+    NoneValueMessage = "Failed to raise ValueError, given None inputs!"
+    # Less idiomatic than pytest.raises, but clearer assertion errors!
+    try:
         score_poker_hands(None, None)
+    except Exception as e:
+        if not isinstance(e, ValueError):
+            raise AssertionError(NoneValueMessage)
+    else:
+        raise AssertionError(NoneValueMessage)
 
 
 @pytest.mark.level(2)
-def test_invalid_empty_inputs():
-    with pytest.raises(ValueError):
-        score_poker_hands("", "")
+@pytest.mark.parametrize("h1, h2", [
+    ("", ""),
+    ("1S", ""),
+    ("", "1S"),
+])
+def test_invalid_empty_inputs(h1, h2):
+    """
+    Empty string inputs raise ValueErrors
+    """
+    EmptyValueMessage = "Failed to raise ValueError, given empty string inputs!"
+    # Less idiomatic than pytest.raises, but clearer assertion errors!
+    try:
+        score_poker_hands(None, None)
+    except Exception as e:
+        if not isinstance(e, ValueError):
+            raise AssertionError(EmptyValueMessage)
+    else:
+        raise AssertionError(EmptyValueMessage)
 
 
 @pytest.mark.level(2)
-def test_invalid_integer_inputs():
-    with pytest.raises(ValueError):
-        score_poker_hands(1, 2)
+@pytest.mark.parametrize("h1, h2", [
+    (1, 2),
+    ("1S", 2),
+    (3.0, "2D"),
+])
+def test_invalid_numeric_inputs(h1, h2):
+    """
+    Numeric inputs raise ValueErrors
+    """
+    NumericValueMessage = "Failed to raise ValueError, given numeric inputs!"
+    # Less idiomatic than pytest.raises, but clearer assertion errors!
+    try:
+        score_poker_hands(None, None)
+    except Exception as e:
+        if not isinstance(e, ValueError):
+            raise AssertionError(NumericValueMessage)
+    else:
+        raise AssertionError(NumericValueMessage)
 
 
 @pytest.mark.level(2)
-def test_invalid_suit_inputs():
-    with pytest.raises(ValueError):
-        score_poker_hands("1Z", "2X")
+@pytest.mark.parametrize("h1, h2", [
+    ("1S", "3X"),
+    ("5H", "2Z"),
+])
+def test_invalid_suit_inputs(h1, h2):
+    """
+    Hands containing invalid suits raise ValueErrors
+    """
+    # Less idiomatic than pytest.raises, but clearer assertion errors!
+    try:
+        score_poker_hands(h1, h2)
+    except Exception as e:
+        if not isinstance(e, ValueError):
+            raise AssertionError(f"Failed to raise ValueError, given invalid Suit {h2[-1]}")
 
 
 @pytest.mark.level(2)
-def test_invalid_rank_inputs():
-    with pytest.raises(ValueError):
-        score_poker_hands("0H", "-1C")
-
+@pytest.mark.parametrize("h1, h2", [
+    ("7D", "0S"),
+    ("4H", "-1C"),
+])
+def test_invalid_rank_inputs(h1, h2):
+    """
+    Hands containing invalid ranks raise ValueErrors
+    """
+    # Less idiomatic than pytest.raises, but clearer assertion errors!
+    try:
+        score_poker_hands(h1, h2)
+    except Exception as e:
+        if not isinstance(e, ValueError):
+            raise AssertionError(f"Failed to raise ValueError, given invalid Rank {h2[0:-1]}")
 
 @pytest.mark.level(3)
 @pytest.mark.parametrize("h1, h2, expected_result", [
@@ -65,12 +150,35 @@ def test_invalid_rank_inputs():
     ("3C QH KC 9C 6H", "4S 7S AD 5D JC", 2),
 ])
 def test_multi_card_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Multi-card hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(4)
 def test_high_single_card_hands_T():
-    assert score_poker_hands("TH", "9S") == 1
+    """
+    T is interpreted to represent Rank 10
+    """
+    assert score_poker_hands("TH", "9S") == 1, "TH should win against 9S!"
+
+@pytest.mark.level(4)
+@pytest.mark.parametrize("h1, h2", [
+    ("10D 3H", "2H 3S"),
+    ("3C QH KC 9C 6H", "4S 7S AD 5D 10C"),
+])
+def test_invalid_obsolete_rank_inputs(h1, h2):
+    """
+    Rank "10" is no longer valid (in favor of rank "T")
+    """
+    # Less idiomatic than pytest.raises, but clearer assertion errors!
+    try:
+        score_poker_hands(h1, h2)
+    except Exception as e:
+        if not isinstance(e, ValueError):
+            raise AssertionError(f"Failed to raise ValueError, given invalid Rank \"10\"")
 
 
 @pytest.mark.level(5)
@@ -79,7 +187,11 @@ def test_high_single_card_hands_T():
     ("3C 5C 1D 3H 4S", "JC 7S 4H AD 4S", 2),
 ])
 def test_pair_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Pair hands are interpreted correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(6)
@@ -89,7 +201,11 @@ def test_pair_hands(h1, h2, expected_result):
     ("3C AC QH 3H 4S", "4C AS 3H QD 3S", 0),
 ])
 def test_pair_ties_whoops(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Hands with equal values are interpreted as a Draw
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(7)
@@ -98,7 +214,11 @@ def test_pair_ties_whoops(h1, h2, expected_result):
     ("3C 5C AD 3H 5S", "JC 6S 4H 6D 4S", 2),
 ])
 def test_two_pair_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Two Pair hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(8)
@@ -107,7 +227,11 @@ def test_two_pair_hands(h1, h2, expected_result):
     ("3C KC 3D 3H 4S", "JC 7S 4H 7D 7H", 2),
 ])
 def test_three_of_a_kind_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Three Of A Kind hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(9)
@@ -117,7 +241,11 @@ def test_three_of_a_kind_hands(h1, h2, expected_result):
     ("3C 5C 1D 4H 2S", "2C 6S 5H 3D 4S", 2),
 ])
 def test_straight_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Straight hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(10)
@@ -126,7 +254,11 @@ def test_straight_hands(h1, h2, expected_result):
     ("7C 5C 1C 3C 4C", "4S 8S 7S 5S 6S", 2),
 ])
 def test_flush_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Flush hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(11)
@@ -135,7 +267,11 @@ def test_flush_hands(h1, h2, expected_result):
     ("3D 3S 4C 3H 4D", "4H 4S 5C 4B 5H", 2),
 ])
 def test_full_house_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Full House hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(12)
@@ -144,7 +280,11 @@ def test_full_house_hands(h1, h2, expected_result):
     ("3C KC 3D 3H 3S", "4H 4S 4C 5H 4B", 2),
 ])
 def test_four_of_a_kind_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Four Of A Kind hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(13)
@@ -153,7 +293,11 @@ def test_four_of_a_kind_hands(h1, h2, expected_result):
     ("3C 5C 1D 3H 4S", "JC 7S 4H AD 4S", 2),
 ])
 def test_straight_hands_whoops(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Straight hands (updated definition!) are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(14)
@@ -162,7 +306,11 @@ def test_straight_hands_whoops(h1, h2, expected_result):
     ("2C 5C 1C 3C 4C", "2S 5S 3S 6S 4S", 2),
 ])
 def test_straight_flush_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Straight Flush hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 
 @pytest.mark.level(15)
@@ -171,13 +319,24 @@ def test_straight_flush_hands(h1, h2, expected_result):
     ("JC AC TC QC KC", "JS AS TS QS KS", 0),
 ])
 def test_royal_flush_hands(h1, h2, expected_result):
-    assert score_poker_hands(h1, h2) == expected_result
+    """
+    Royal Flush hands are evaluated correctly
+    """
+    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
+    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
 
 @pytest.mark.level(16)
 @pytest.mark.parametrize("h1, h2", [
-    ("JC AC AC QC KC", "2S 5S 3S 6S 4S"),
-    ("JC AC TC QC KC", "JS AC TS QS KS"),
+    ("JC AC AC 1C 8D", "2S 5D 3S 6H 4S"),
+    ("JC AC TC 1C 8D", "JS AC TS 6H 4S"),
 ])
 def test_duplicate_cards(h1, h2):
-    with pytest.raises(ValueError):
-        assert score_poker_hands(h1, h2)
+    """
+    Duplicate cards within/across hands raise ValueErrors
+    """
+    # Less idiomatic than pytest.raises, but clearer assertion errors!
+    try:
+        score_poker_hands(h1, h2)
+    except Exception as e:
+        if not isinstance(e, ValueError):
+            raise AssertionError(f"Failed to raise ValueError, given duplicate card AC!")
