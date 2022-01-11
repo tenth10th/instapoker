976e71bc44949f7bc3ee087ab916586df837c5a4
David Sturgis <tenth10th@gmail.com>
2022-01-11 02:20:10 -0500

    Co-authored-by: rod <rrhg@users.noreply.github.com> Co-authored-by: Dave McConeghy <dmcconeghy@users.noreply.github.com> Co-authored-by: LewisStaples <LewisStaples@users.noreply.github.com> Co-authored-by: Babar Baig <BobBaig@outlook.com> Co-authored-by: loyeleye <loyeleye@users.noreply.github.com> Co-authored-by: dsher

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,10 +1,102 @@
+def parse_hand(hand_string):
+    """
+    Turns hand string into a list of card string(s)
+    """
+    # Move this into the top of parse_hand (as the first step)
+    if not isinstance(hand_string, str):
+        raise WhoopsiePoopsie("Not a string.")
+
+    list_of_cards = hand_string.split(" ") # Returns a list of 1 card, if there aren't spaces in hand_string...
+    for card in list_of_cards:
+        validate(card) #TODO: change to validate_card
+
+    return list_of_cards
+
+
+def card_parser(card_string):
+    """
+    Given a string representation of a card, like "2H", determine a value
+    (Which we can use to compare, and find a "winner")
+    """
+    face_card_map = {"J": 11, "Q": 12, "K": 13, "A": 14}
+    value = card_string[0:-1]
+
+    face_value = face_card_map.get(value.upper())
+
+    if face_value:
+        return face_value
+
+    #raise ValueError("Whoopsie poopsie: Invalid Hand Type (expected String)")
+    #raise ValueError("Whoopsie poopsie: Invalid Suit")
+    #raise ValueError("Whoopsie poopsie: Invalid Rank")
+
+    print("card_string: <",repr(card_string),">")
+    return int(card_string[0:-1])
+
+
+def compare_cards(card_string1, card_string2):
+    """
+    Given two cards such as "3H" and "4C", determine which has greater value.
+    Given two poker hands, represented by strings, return an integer result:
+        0: Draw (hands have equal value)
+        1: Hand 1 Wins
+        2: Hand 2 Wins
+    """
+    card_one = card_parser(card_string1) 
+    card_two = card_parser(card_string2)
+    
+    if card_one < card_two:
+        return 2
+    if card_one > card_two:
+        return 1
+    return 0
+
+class WhoopsiePoopsie(ValueError):
+    pass
+
+
+def validate(hand):
+    # Dave's Proposal:
+
+    # Once you have the List of Cards, e.g. ["2H", "4C"], then iterate them,
+    # and run both of these checks on each card:
+    if hand[-1] not in 'CHSD':
+        raise WhoopsiePoopsie("Invalid suit.")
+    if hand[0:-1] not in {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}:
+        raise WhoopsiePoopsie("Invalid rank.")
+    # And THEN, if we haven't Exception'ed out, return the hand...
+
+
+# idea dictionary with result_array[value,card]
+def get_highest_card(hand):
+    max_value = 1
+    for card in hand:
+        print("<",repr(card),">")
+        value = card_parser(card)
+        max_value = max(max_value, value)
+    return max_value
+
+
 def score_poker_hands(h1, h2):
     """
     Given two poker hands, represented by strings, return an integer result:
         0: Draw (hands have equal value)
         1: Hand 1 Wins
         2: Hand 2 Wins
+
+    Before: "2H"
+    After: "3H 1D JC" vs "6S 10H 2A" (first hand wins - highest card Jack)
     """
-    # TODO: Figure out how Poker hands are scored?
-    # FIXME: Implement Poker scoring here, I guess
-    pass
+    card_list_1 = parse_hand(h1)
+    card_list_2 = parse_hand(h2)
+
+    # TODO: Are these no longer needed? (now that parse_hand validates?)
+    for card in card_list_1:
+        validate(card)
+        
+    for card in card_list_2:
+        validate(card)
+
+    result = compare_cards(h1, h2)
+
+    return result
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,5 +1,61 @@
-from poker_scoring_api import score_poker_hands
+from poker_scoring_api import (
+    score_poker_hands, card_parser, compare_cards, parse_hand, 
+    get_highest_card)
 
-def test_example_unit_test():
-    # FIXME: Maybe assert something meaningful about score_poker_hands?
-    assert score_poker_hands is not None
+import pytest
+
+
+def test_card_parser():
+    assert card_parser("2H") == 2
+
+def test_compare_cards_less():
+    assert compare_cards("4H", "3C") == 1
+
+def test_compare_cards_equal():
+    assert compare_cards("5H", "5S") == 0
+
+def test_compare_cards_greater():
+    assert compare_cards("5H", "6S") == 2
+
+def test_card_parse_face_cards_jack():
+    assert card_parser("JH") == 11
+    assert card_parser("jH") == 11
+
+def test_card_parse_face_cards_queen():
+    assert card_parser("QH") == 12
+    assert card_parser("qH") == 12
+
+def test_card_parse_face_cards_king():
+    assert card_parser("KH") == 13
+    assert card_parser("kH") == 13
+
+def test_card_parse_face_cards_ace():
+    assert card_parser("AH") == 14
+    assert card_parser("aH") == 14
+
+def test_card_JS_should_lose_against_QH():
+    assert score_poker_hands("JS", "QH") == 2
+
+def test_invalid_card_not_string():
+    with pytest.raises(ValueError):
+        assert score_poker_hands(0, 0)
+
+def test_invalid_suit():
+    with pytest.raises(ValueError, match = "Invalid suit."):
+        assert score_poker_hands("2Z","2Z")
+
+def test_invalid_rank():
+    with pytest.raises(ValueError, match = "Invalid rank."):
+        assert score_poker_hands("ZH","14H")
+
+def test_parse_hand():
+    assert parse_hand("3H 2D JC") == ["3H", "2D", "JC"]
+
+def test_parse_single_hand():
+    assert parse_hand("3H") == ["3H"]
+
+# def test_multi_card_hand():
+#     assert score_poker_hands("3H 1D JC", "6S 10H 2C") == 1
+
+def test_get_highest_card():
+    assert get_highest_card(["6S", "10H", "2C"]) == 10
