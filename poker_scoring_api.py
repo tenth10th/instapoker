c5e6e0bfbf4e93ff0f03893ccc8ecdd84fcb22d2
David Sturgis <tenth10th@gmail.com>
2022-01-11 17:29:16 +0000

    Clean up starter `score_poker_hands`
    Restore the initial `score_poker-hands` function to its intended, extremely minimal starting state.

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,10 +1,9 @@
-def score_poker_hands(h1, h2):
+def score_poker_hands():  # FIXME: This should take arguments?
     """
     Given two poker hands, represented by strings, return an integer result:
-        0: Draw (hands have equal value)
         1: Hand 1 Wins
         2: Hand 2 Wins
     """
-    # TODO: Figure out how Poker hands are scored?
-    # FIXME: Implement Poker scoring here, I guess
+    # FIXME: Implement Poker scoring here, I guess...
     pass
+    # TODO: Figure out how Poker hands are supposed to be scored?
