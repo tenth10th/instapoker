779456ea7731eb0cf79df212f2610d2f335f49e9
David Sturgis <tenth10th@gmail.com>
2022-02-07 00:02:31 -0500

    Refactor markers, add integration "fast-fail"
    Replace the separate "level" and "max_level" markers with a single "integration" marker that accepts both positional and keyword arguments.
    (pargs will be checked first, assuming min_level, optionally followed by max_level, and if no args are present, it looks for explicit `min_level` and `max_level` kwargs). `min_level` and `max_level` both default to None. (An Integration test with a level range of (None, None) will always run on `--submit`, regardless of level.)
    
    Replace `get_level_marks` with `get_integration_levels`, a function that returns an Integration object, which is overall truthy for integration tests, and falsey for user-space tests, (and includes an explicit `is_integration` boolean), and contains `min_level` and `max_level` properties.
    
    All integration tests have been updated to use the new `integration` mark. Added a new "first" integration test to verify that the `score_poker_hands` function exists.
    
    Added a pytest_runtest_makereport hook so that, in submit mode, PyTest will exit after the first failed Integration test.
    
    Added `pytest-pspec` to the requirements, to render more "legible" stack traces. Extended the `pytest_runtest_makereport` hook to force stack traces into "one line" mode for Integration tests (if they fail on AssertionErrors, or manual pytest.fail() calls - We still want to see full tracebacks if an Integration Test causes the userspace code to raise runtime exceptions.

--- conftest.py
+++ conftest.py
@@ -1,22 +1,35 @@
+from dataclasses import dataclass, field
 import json
 from os import write
 from os.path import exists
 import pytest
+from _pytest.reports import TestReport
 from spoiler_alert_keep_out.level_documentation import (
     display_boss_email,
     display_poker_rules,
-    display_test_level_info,
 )
 
 debug = False
 
-MARK_LEVEL = 'level'
-MARK_MAX_LEVEL = 'max_level'
-ALL_LEVEL_MARKS = (MARK_LEVEL, MARK_MAX_LEVEL)
+MIN_LEVEL = 'min_level'
+MAX_LEVEL = 'max_level'
+INTEGRATION = 'integration'
+ALL_LEVEL_MARKS = (MIN_LEVEL, MAX_LEVEL)
 
 LEVEL_STATE_PATH = 'spoiler_alert_keep_out/level_state.json'
 
 level_state = {}
+
+@dataclass
+class Integration():
+    """
+    Describes integration test status, and level ranges (if applicable)
+    """
+    is_integration: bool = False
+    min_level: int = None
+    max_level: int = None
+    def __bool__(self):
+        return self.is_integration
 
 
 def pytest_addoption(parser):
@@ -24,7 +37,7 @@ def pytest_addoption(parser):
         "--level",
         action="store",
         metavar="LEVEL_INT",
-        help="Override level, to view older emails",
+        help="Override level, e.g. to view older emails",
     )
     parser.addoption(
         "--email",
@@ -45,14 +58,10 @@ def pytest_addoption(parser):
 
 def pytest_configure(config):
     """
-    Register an additional "level" marker that takes a single integer argument
+    Register an additional "integration" marker that takes a single integer argument
     """
     config.addinivalue_line(
-        "markers", "level(level_int): Run only at a certain testing level or higher"
-    )
-
-    config.addinivalue_line(
-        "markers", "max_level(max_level_int): Maximum level cap to run a level-ed test"
+        "markers", "integration(min_level, max_level): Run in --submit mode, if in appropriate level range"
     )
 
     level = 0
@@ -90,12 +99,28 @@ def pytest_configure(config):
         pytest.exit(f"(after displaying v{(level or 0)+1} Poker rules)")
 
 
-def get_level_marks(item):
-    level_marks = {
-        mark.name: mark.args[0] for mark in item.iter_markers()
-        if mark.name in ALL_LEVEL_MARKS
-    }
-    return level_marks.get(MARK_LEVEL), level_marks.get(MARK_MAX_LEVEL)
+def get_integration_levels(item):
+    """
+    Return (min_level, max_level), first checking args, then kwargs.
+    Both min_level and max_level default to None if undefined.
+    """
+    integration_marks = [
+        mark for mark in item.iter_markers()
+        if mark.name == INTEGRATION
+    ]
+    if not integration_marks:
+        return Integration(is_integration=False)
+
+    mark = integration_marks[0]
+    levels = dict()
+    if mark.args:
+        levels[MIN_LEVEL] = mark.args[0]
+        levels[MAX_LEVEL] = mark.args[1] if len(mark.args > 1) else None
+    else:
+        levels[MIN_LEVEL] = mark.kwargs.get(MIN_LEVEL)
+        levels[MAX_LEVEL] = mark.kwargs.get(MAX_LEVEL)
+
+    return Integration(is_integration=True, **levels)
 
 
 def pytest_runtest_setup(item):
@@ -114,30 +139,32 @@ def pytest_runtest_setup(item):
         except ValueError:
             pytest.exit(f"Invalid current_level config option: {level}")
 
-    min_level, max_level = get_level_marks(item)
+    integration = get_integration_levels(item)
 
-    if not submit and min_level is not None or max_level is not None:
+    if integration and not submit:
         pytest.skip("(Not Running any Integration Tests)")
-    if min_level and min_level > level:
-        pytest.skip("Not Until L{}".format(min_level))
-    if max_level and level > max_level:
-        pytest.skip("Not After L{}".format(max_level))
+    if submit and integration:
+        item.config.tb = 'line'
+        if integration.min_level and integration.min_level > level:
+            pytest.skip("Not Until L{}".format(integration.min_level))
+        if integration.max_level and level > integration.max_level:
+            pytest.skip("Not After L{}".format(integration.max_level))
 
 
 def pytest_sessionfinish(session, exitstatus):
-    print("\n")
     status_int = int(exitstatus)
     submit = session.config.getoption("--submit")
     level_state = load_level_state()
     if debug:
+        print("\n")
         print(f"status_int: {status_int}")
         print(f"submit active: {submit}")
     if submit and status_int == 0:
         level_state['current_level'] += 1
         write_level_state(level_state)
-        print(f"* * * All Tests Passed: Advancing to level {level_state['current_level']}! * * *")
+        print(f"\n\n* * * All Tests Passed: Advancing to level {level_state['current_level']}! * * *")
     elif submit:
-        print(f"(Some Tests Failed - Remaining on level {level_state['current_level']})")
+        print(f"\n\n(Some Tests Failed - Remaining on level {level_state['current_level']})")
 
 
 def load_level_state():
@@ -164,3 +191,32 @@ def write_level_state(level_state):
         except Exception as e:
             print("FAILED TO WRITE CONFIG!")
             print(e)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """
+    Post-Test reporting behaviors
+
+    * Stop with failure after the first Integration test fails
+
+    * Render Integration test failures in "single line" traceback mode
+    """
+    test_run = yield
+    integration = get_integration_levels(item)
+    if not integration:
+        return
+
+    result = test_run.get_result()
+    if result.outcome == 'failed':
+        item.session.shouldfail = "(Integration Test Failed)"
+        # Re-render the result repr with single-line tracebacks
+        one_liner = item._repr_failure_py(
+            call.excinfo,
+            style="line"
+        )
+        one_liner_str = str(one_liner)
+        print("one_liner:", one_liner)
+        if "AssertionError" in one_liner_str or "E   Failed" in one_liner_str:
+            result.longrepr = one_liner
+    return result
--- spoiler_alert_keep_out/test_integrations.py
+++ spoiler_alert_keep_out/test_integrations.py
@@ -1,6 +1,51 @@
 from random import expovariate
+import inspect
 import pytest
-from poker_scoring_api import score_poker_hands
+
+"""
+Integration Tests
+"""
+
+try:
+    import poker_scoring_api
+except ImportError:
+    poker_scoring_api = None
+
+
+@pytest.mark.integration
+def test_poker_scoring_api_module_exists():
+    """
+    poker_scoring_api.py is present
+    """
+    assert poker_scoring_api is not None, (
+        "poker_scoring_api.py module must be present!"
+    )
+
+
+@pytest.mark.integration
+def test_score_poker_hands_exists():
+    """
+    poker_scoring_api.py contains score_poker_hands
+    """
+    assert hasattr(poker_scoring_api, "score_poker_hands"), (
+        "poker_scoring_api.py must contain a \"score_poker_hands\" function"
+    )
+
+
+try:
+    from poker_scoring_api import score_poker_hands
+except ImportError:
+    score_poker_hands = None
+
+
+@pytest.mark.integration
+def test_score_poker_hands_is_a_function():
+    """
+    poker_scoring_api.score_poker_hands is a function
+    """
+    assert inspect.isfunction(poker_scoring_api.score_poker_hands), (
+        "poker_scoring_api.score_poker_hands must be a function!"
+    )
 
 
 def hand_error_str(h1, h2, int_result):
@@ -13,17 +58,30 @@ def hand_error_str(h1, h2, int_result):
 
     return f"{h1} should {result_verb} against {h2}!"
 
+def validate_hand_result(h1, h2, expected_result):
+    __tracebackhide__ = True
+    result = score_poker_hands(h1, h2)
+    if result != expected_result:
+        pytest.fail(hand_error_str(h1, h2, expected_result))
 
-@pytest.mark.level(0)
+@pytest.mark.integration(min_level=0)
 def test_poker_scoring_api():
     """
-    score_poker_hands accepts two strings, and returns an integer
+    score_poker_hands accepts two strings
+    """
+    result = score_poker_hands("2D", "2H")
+
+
+@pytest.mark.integration(min_level=0)
+def test_poker_scoring_api_return_type():
+    """
+    score_poker_hands returns an integer
     """
     result = score_poker_hands("2D", "2H")
     assert isinstance(result, int), "score_poker_hands output must be an integer!"
 
 
-@pytest.mark.level(1)
+@pytest.mark.integration(min_level=1)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3H", "2D", 1),
     ("4C", "5S", 2),
@@ -35,20 +93,18 @@ def test_single_card_hands(h1, h2, expected_result):
     """
     Single-card hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
 
-@pytest.mark.level(1)
-@pytest.mark.max_level(3)
+@pytest.mark.integration(min_level=1, max_level=3)
 def test_high_single_card_hands_10():
     """
     Single-card rank 10 hands are evaluated correctly
     """
-    assert score_poker_hands("10H", "9S") == 1, "10H should win against 9S!"
+    validate_hand_result("10H", "9S", 1)
 
 
-@pytest.mark.level(2)
+@pytest.mark.integration(min_level=2)
 @pytest.mark.parametrize("h1, h2", [
     (None, None),
     ("1S", None),
@@ -69,7 +125,7 @@ def test_invalid_None_inputs(h1, h2):
         raise AssertionError(NoneValueMessage)
 
 
-@pytest.mark.level(2)
+@pytest.mark.integration(min_level=2)
 @pytest.mark.parametrize("h1, h2", [
     ("", ""),
     ("1S", ""),
@@ -90,7 +146,7 @@ def test_invalid_empty_inputs(h1, h2):
         raise AssertionError(EmptyValueMessage)
 
 
-@pytest.mark.level(2)
+@pytest.mark.integration(min_level=2)
 @pytest.mark.parametrize("h1, h2", [
     (1, 2),
     ("1S", 2),
@@ -111,7 +167,7 @@ def test_invalid_numeric_inputs(h1, h2):
         raise AssertionError(NumericValueMessage)
 
 
-@pytest.mark.level(2)
+@pytest.mark.integration(min_level=2)
 @pytest.mark.parametrize("h1, h2", [
     ("1S", "3X"),
     ("5H", "2Z"),
@@ -131,7 +187,7 @@ def test_invalid_suit_inputs(h1, h2):
         raise AssertionError(assertion_error)
 
 
-@pytest.mark.level(2)
+@pytest.mark.integration(min_level=2)
 @pytest.mark.parametrize("h1, h2", [
     ("7D", "0S"),
     ("4H", "-1C"),
@@ -150,27 +206,25 @@ def test_invalid_rank_inputs(h1, h2):
     else:
         raise AssertionError(assertion_error)
 
-@pytest.mark.level(3)
+@pytest.mark.integration(min_level=3)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("4D 3H", "2H 3S", 1),
-    ("3C QH KC 9C 6H", "4S 7S AD 5D JC", 2),
+    ("4C QH KC 9C 6H", "3S 7S AD 5D JC", 2),
 ])
 def test_multi_card_hands(h1, h2, expected_result):
     """
     Multi-card hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(4)
+@pytest.mark.integration(min_level=4)
 def test_high_single_card_hands_T():
     """
     T is interpreted to represent Rank 10
     """
-    assert score_poker_hands("TH", "9S") == 1, "TH should win against 9S!"
+    validate_hand_result("TH", "9S", 1)
 
-@pytest.mark.level(4)
+@pytest.mark.integration(min_level=4)
 @pytest.mark.parametrize("h1, h2", [
     ("10D 3H", "2H 3S"),
     ("3C QH KC 9C 6H", "4S 7S AD 5D 10C"),
@@ -189,7 +243,7 @@ def test_invalid_obsolete_rank_inputs(h1, h2):
     else:
         raise AssertionError(assertion_error)
 
-@pytest.mark.level(5)
+@pytest.mark.integration(min_level=5)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3C 5C 1D 3H 4S", "4S 7S AD 5D JC", 1),
     ("3C 5C 1D 3H 4S", "JC 7S 4H AD 4S", 2),
@@ -198,11 +252,9 @@ def test_pair_hands(h1, h2, expected_result):
     """
     Pair hands are interpreted correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(6)
+@pytest.mark.integration(min_level=6)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3C AC 1D 3H 4S", "JC 7S 4H QD 4S", 2),
     ("3C AC QH 3H 4S", "5C AS 3H QD 3S", 2),
@@ -212,11 +264,9 @@ def test_pair_ties_whoops(h1, h2, expected_result):
     """
     Hands with equal values are interpreted as a Draw
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(7)
+@pytest.mark.integration(min_level=7)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3C 5C 1D 3H 5S", "4S 7S 4D 5D JC", 1),
     ("3C 5C AD 3H 5S", "JC 6S 4H 6D 4S", 2),
@@ -225,11 +275,9 @@ def test_two_pair_hands(h1, h2, expected_result):
     """
     Two Pair hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(8)
+@pytest.mark.integration(min_level=8)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3C 5C 3D 3H 4S", "JC 7S 4H 7D 4S", 1),
     ("3C KC 3D 3H 4S", "JC 7S 4H 7D 7H", 2),
@@ -238,12 +286,9 @@ def test_three_of_a_kind_hands(h1, h2, expected_result):
     """
     Three Of A Kind hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(9)
-@pytest.mark.max_level(12)
+@pytest.mark.integration(min_level=9, max_level=12)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3C 5C 1D 4H 2S", "JC 7S 4H 7D 7H", 1),
     ("3C 5C 1D 4H 2S", "2C 6S 5H 3D 4S", 2),
@@ -252,11 +297,9 @@ def test_straight_hands(h1, h2, expected_result):
     """
     Straight hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(10)
+@pytest.mark.integration(min_level=10)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("7C 5C 1C 3C 4C", "4C 8S 7H 5D 6S", 1),
     ("7C 5C 1C 3C 4C", "4S 8S 7S 5S 6S", 2),
@@ -265,11 +308,9 @@ def test_flush_hands(h1, h2, expected_result):
     """
     Flush hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(11)
+@pytest.mark.integration(min_level=11)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3D 3S 4C 3H 4D", "2C 5C 1C 3C 4C", 1),
     ("3D 3S 4C 3H 4D", "4H 4S 5C 4B 5H", 2),
@@ -278,11 +319,9 @@ def test_full_house_hands(h1, h2, expected_result):
     """
     Full House hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(12)
+@pytest.mark.integration(min_level=12)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3C 5C 3D 3H 3S", "4H 4S 5C 4B 5H", 1),
     ("3C KC 3D 3H 3S", "4H 4S 4C 5H 4B", 2),
@@ -291,11 +330,9 @@ def test_four_of_a_kind_hands(h1, h2, expected_result):
     """
     Four Of A Kind hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(13)
+@pytest.mark.integration(min_level=13)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("3C 5C 1D 3H 4S", "4S 7S AD 5D JC", 1),
     ("3C 5C 1D 3H 4S", "JC 7S 4H AD 4S", 2),
@@ -304,11 +341,9 @@ def test_straight_hands_whoops(h1, h2, expected_result):
     """
     Straight hands (updated definition!) are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-
-@pytest.mark.level(14)
+@pytest.mark.integration(min_level=14)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("2C 5C 1C 3C 4C", "JC 7S 4H AD 4S", 1),
     ("2C 5C 1C 3C 4C", "2S 5S 3S 6S 4S", 2),
@@ -317,11 +352,10 @@ def test_straight_flush_hands(h1, h2, expected_result):
     """
     Straight Flush hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
 
-@pytest.mark.level(15)
+@pytest.mark.integration(min_level=15)
 @pytest.mark.parametrize("h1, h2, expected_result", [
     ("JC AC TC QC KC", "2S 5S 3S 6S 4S", 1),
     ("JC AC TC QC KC", "JS AS TS QS KS", 0),
@@ -330,10 +364,10 @@ def test_royal_flush_hands(h1, h2, expected_result):
     """
     Royal Flush hands are evaluated correctly
     """
-    hand_assertion_error_message = hand_error_str(h1, h2, expected_result)
-    assert score_poker_hands(h1, h2) == expected_result, hand_assertion_error_message
+    validate_hand_result(h1, h2, expected_result)
 
-@pytest.mark.level(16)
+
+@pytest.mark.integration(min_level=16)
 @pytest.mark.parametrize("h1, h2", [
     ("JC AC AC 1C 8D", "2S 5D 3S 6H 4S"),
     ("JC AC TC 1C 8D", "JS AC TS 6H 4S"),
