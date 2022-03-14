afb93465a5c6983e5b92dc17182971ccf257fb43
David Sturgis <david-sturgis@pluralsight.com>
2022-03-14 02:52:44 +0000

    Progress through Level 3
    Co-authored-by: Jenny Carlston <jenny-carlston@pluralsight.com>
    Co-authored-by: Bart Petersen <barton-petersen@pluralsight.com>
    Co-authored-by: Sean Quinn <sean-quinn@pluralsight.com>

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,4 +1,29 @@
-def score_poker_hands(): # FIXME: Accept some args?
-    # TODO: Poker stuff here?
-    # FIXME: Return something?
-    pass
+rank_values = {str(x):x for x in range(2, 11)}
+rank_values['J'] = 11
+rank_values['Q'] = 12
+rank_values['K'] = 13
+rank_values['A'] = 14
+
+suit_values = "CDHS"
+
+
+def score_poker_hands(first_hand: str, second_hand: str):
+    if first_hand is None or second_hand is None:
+        raise ValueError("Invalid None hands:", first_hand, second_hand)
+
+    first_rank = first_hand[0:-1]
+    second_rank = second_hand[0:-1]
+    
+    if first_rank not in rank_values or second_rank not in rank_values:
+        raise ValueError("Invalid rank in hands:", first_hand, second_hand)
+
+    first_suit = first_hand[-1]
+    second_suit = second_hand[-1]
+    
+    if first_suit not in suit_values or second_suit not in suit_values:
+        raise ValueError("Invalid suit in hands:", first_hand, second_hand)
+
+    if (int(first_rank) > int(second_rank)):
+        return 1
+    else:
+        return 2
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,61 +1,38 @@
-from poker_scoring_api import (
-    score_poker_hands, card_parser, compare_cards, parse_hand, 
-    get_highest_card)
-
+from poker_scoring_api import score_poker_hands
 import pytest
 
 
-def test_card_parser():
-    assert card_parser("2H") == 2
+def test_first_hand_wins():
+    actual = score_poker_hands("10H", "2H")
+    assert isinstance(actual, int)
+    assert actual == 1
 
-def test_compare_cards_less():
-    assert compare_cards("4H", "3C") == 1
 
-def test_compare_cards_equal():
-    assert compare_cards("5H", "5S") == 0
+def test_second_hand_wins():
+    actual = score_poker_hands("2H", "10H")
+    assert isinstance(actual, int)
+    assert actual == 2
 
-def test_compare_cards_greater():
-    assert compare_cards("5H", "6S") == 2
 
-def test_card_parse_face_cards_jack():
-    assert card_parser("JH") == 11
-    assert card_parser("jH") == 11
-
-def test_card_parse_face_cards_queen():
-    assert card_parser("QH") == 12
-    assert card_parser("qH") == 12
-
-def test_card_parse_face_cards_king():
-    assert card_parser("KH") == 13
-    assert card_parser("kH") == 13
-
-def test_card_parse_face_cards_ace():
-    assert card_parser("AH") == 14
-    assert card_parser("aH") == 14
-
-def test_card_JS_should_lose_against_QH():
-    assert score_poker_hands("JS", "QH") == 2
-
-def test_invalid_card_not_string():
+@pytest.mark.parametrize("first_hand, second_hand", [
+    ("4D", "0D"),
+    ("0D", "4D"),
+    ("14D", "4D"),
+    (None, "2D"),
+    ("4D", None),
+    ("1D", "2D")
+])
+def test_invalid_ranks(first_hand, second_hand):
     with pytest.raises(ValueError):
-        assert score_poker_hands(0, 0)
+        score_poker_hands(first_hand, second_hand)
 
-def test_invalid_suit():
-    with pytest.raises(ValueError, match = "Invalid suit."):
-        assert score_poker_hands("2Z","2Z")
 
-def test_invalid_rank():
-    with pytest.raises(ValueError, match = "Invalid rank."):
-        assert score_poker_hands("ZH","14H")
-
-def test_parse_hand():
-    assert parse_hand("3H 2D JC") == ["3H", "2D", "JC"]
-
-def test_parse_single_hand():
-    assert parse_hand("3H") == ["3H"]
-
-def test_multi_card_hand():
-    assert score_poker_hands("3H AD JC", "6S 10H 2C") == 1
-
-def test_get_highest_card():
-    assert get_highest_card(["6S", "10H", "2C"]) == "10H"
+@pytest.mark.parametrize("first_hand, second_hand", [
+    ("4D", "4X"),
+    ("4X", "4D"),
+    ("10DD", "10D"),
+    ("10D", "2DD"),
+])
+def test_invalid_suits(first_hand, second_hand):
+    with pytest.raises(ValueError):
+        score_poker_hands(first_hand, second_hand)
