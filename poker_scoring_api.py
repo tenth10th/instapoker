f5ed26ab4563b9478b4be71adf51210d4e49035a
David Sturgis <tenth10th@gmail.com>
2024-02-13 02:48:08 +0000

    Work in progress from BSC February 2024 session

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,229 +1,56 @@
-from dataclasses import dataclass, field
-from functools import total_ordering
+def score_poker_hands(hand1, hand2):
 
-DRAW = 0
-WIN_HAND_1 = 1
-WIN_HAND_2 = 2
-
-@total_ordering
-@dataclass
-class Card:
-    """
-    Cards have ranks and suits. 
-    This is a validated representation. 
-    """
-    name: str
-    rank: int = field(init=False)
-    suit: str = field(init=False)
-
-    def __post_init__(self):
-        self.validate_card()
-        self.parse_rank()
-        self.parse_suit()
-
-    def validate_card(self):
-        """
-        Validate a card, raising an exception if invalid
-        """    
-        if self.name[0:-1] not in {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}:
-            raise InvalidPlayerHandException(f"Invalid rank: {self.name[0:-1]}")
-
-        if self.name[-1] not in 'CHSD':
-            print(self.name[-1])
-            raise InvalidPlayerHandException(f"Invalid suit: {self.name[-1]}")
-
-    def parse_rank(self):
-        face_card_map = {"T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
-        value = self.name[0:-1]
-
-        face_value = face_card_map.get(value.upper())
-
-        if face_value:
-            self.rank = face_value
-        else:
-            self.rank = int(self.name[0:-1])
+    # Validate
+    if((not isinstance(hand1,str)) or (not isinstance(hand2,str))):
+        raise ValueError("hand is not a string")
+    rank1 = getRank(hand1)
+    suit1 = getSuit(hand1)
+    rank2 = getRank(hand2)
+    suit2 = getSuit(hand2)
+    if(rank1 is None):
+        raise ValueError("hand1 invalid rank")
+    if(rank2 is None):
+        raise ValueError("hand2 invalid rank")
+    if(suit1 is None):
+        raise ValueError("hand1 invalid suit")
+    if(suit2 is None):
+        raise ValueError("hand2 invalid suit")
     
-    def parse_suit(self):
-        self.suit = self.name[-1]
+    #idea1: create a dictionary
+    #key (string values): 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+    #values             : 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 
+    #idea2: to have "rankSuit"  2H as the key (into integer)
+    #create a func getRank()
 
-    def __lt__(self, other):
-        if not isinstance(other, Card):
-            raise NotImplementedError('Not a card')
-        return self.rank < other.rank
-
-    def __eq__(self, other):
-        if not isinstance(other, Card):
-            raise NotImplementedError('Not a card')
-        return self.rank == other.rank
-       
-
-def score_poker_hands(h1, h2):
-    """
-    Given two poker hands, represented by strings, return an integer result:
-        0: Draw (hands have equal value)
-        1: Hand 1 Wins
-        2: Hand 2 Wins
-
-    Before: "2H"
-    After: "3H 1D JC" vs "6S TH 2A" (first hand wins - highest card Jack)
-    """
-    pair_results = pair_beats_non_pair(h1, h2)
-    
-    if pair_results:
-        return pair_results
-
-    card_list_1 = parse_hand(h1)
-    card_list_2 = parse_hand(h2)
-
-    sorted_hand_1 = get_sorted_hand(card_list_1)
-    sorted_hand_2 = get_sorted_hand(card_list_2)
-
-    return compare_rank_lists (sorted_hand_1, sorted_hand_2)
-
-def parse_hand(hand_string):
-    """
-    Turns hand string into a list of card string(s)
-    UPDATE ME to use the new Cards class!
-    """
-    # Move this into the top of parse_hand (as the first step)
-    if not isinstance(hand_string, str):
-        raise InvalidPlayerHandException("Not a string.")
-
-    list_of_cards = hand_string.split(" ") # Returns a list of 1 card, if there aren't spaces in hand_string...
-    for card in list_of_cards:
-        validate_card(card)
-
-    return list_of_cards
-
-
-def card_parser(card_string):
-    """
-    (DEPRECATED - Should be replaced with new Card class)
-    Given a string representation of a card, like "2H", determine a value
-    (Which we can use to compare, and find a "winner")
-    """
-    face_card_map = {"T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
-    value = card_string[0:-1]
-
-    face_value = face_card_map.get(value.upper())
-
-    if face_value:
-        return face_value
-
-    # print("card_string: <",repr(card_string),">")
-    try:
-        return int(card_string[0:-1])
-    except Exception:
-        print(card_string[0:-1])
-        raise
-
-
-def compare_cards(card_string1, card_string2):
-    """
-    (DEPRECATED, NO LONGER IN USE - Card does its own parsing, and is comparable)
-    Given two cards such as "3H" and "4C", determine which has greater value.
-    Given two poker hands, represented by strings, return an integer result:
-        1: Hand 1 Wins
-        2: Hand 2 Wins
-        0: Draw or Tie
-    """
-    card_one = card_parser(card_string1)
-    card_two = card_parser(card_string2)
-    
-    if card_one < card_two:
-        return WIN_HAND_2
-    if card_one > card_two:
-        return WIN_HAND_1
-    return DRAW
-
-
-class InvalidPlayerHandException(ValueError):
-    pass
-
-
-
-def validate_card(card):
-    """
-    DEPRECATED - This behavior is built into new Card class
-    Validate a card, raising an exception if invalid
-    """    
-    if card[-1] not in 'CHSD':
-        raise InvalidPlayerHandException("Invalid suit.")
-    if card[0:-1] not in {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}:
-        raise InvalidPlayerHandException("Invalid rank the card was: " + card)
-    
-
-# idea dictionary with result_array[value,card]
-def get_sorted_hand(card_list):
-    """
-    There is still value in sorting lists of Card instances!
-    But we no longer need to parse them in the process if we're starting with Cards
-    """
-    array = list()
-    for card in card_list:
-        array.append(card_parser(card))
-    array.sort(reverse=True)
-    return array
-
-
-def compare_rank_lists(rank_list_1, rank_list_2):
-    """
-    Return results for two sorted lists
-    (should already work for lists of Card classes, since Cards are comparable?)
-    """
-    if rank_list_1 > rank_list_2:
-        return WIN_HAND_1
-    elif rank_list_1 < rank_list_2:
-        return WIN_HAND_2
-    return DRAW
-
-
-def has_pair(hand):
-    """
-    Returns True if hand contains a Pair
-    (Currently takes a raw string representation of a hand)
-    (Should be refactored to use Lists of Cards, or new Hand class TBD?)
-    """
-    list_of_cards = parse_hand(hand)
-    set_of_ranks = set()
-    for card in list_of_cards:
-        set_of_ranks.add(card[0])
-    print("list_of_cards:", list_of_cards)
-    print("set_of_ranks:", set_of_ranks)
-    print("list length, set length:", len(list_of_cards), len(set_of_ranks))
-    return len(list_of_cards) != len(set_of_ranks)
-
-
-def compare_pair_hands(hand1, hand2):
-    """
-    THIS FUNCTION IS NOT FULLY IMPLEMENTED YET
-    (See commented-out test at the bottom of test_poker_scoring.py)
-    Return 0, 1, or 2, to indicate a winning hand (or a draw) based on pairs
-    potentially a "better" version of pair_beats_non_pair
-    """
-    pair_map1 = dict()
-    pair_map2 = dict()
-    for card in get_sorted_hand(hand1):
-        if card in pair_map1:
-            pair_map1[card] += 1
-        else:
-            pair_map1[card] = 1    
-    for card in get_sorted_hand(hand2):
-        if card in pair_map2:
-            pair_map2[card] += 1
-        else:
-            pair_map2[card] = 1
-    print('map1: ', end='')
-    print(pair_map1)
-    print('map2: ', end='')
-    print(pair_map2)
-
-def pair_beats_non_pair(h1, h2):
-    pair1 = has_pair(h1)
-    pair2 = has_pair(h2)
-
-    if pair1 and not pair2:
+    if(getRank(hand1) > getRank(hand2) ):
         return 1
-    if pair2 and not pair1:
+    else:
         return 2
-    return 0
+
+def getRank(hand):
+    str_rank = hand[:-1]
+    
+    # Dictionary seems like the simplest way to
+    # associate strings with int values (and look up
+    # those associations easily)
+    rank_dict = {
+        "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+        "7": 7, "8": 8, "9": 9, "10": 10, 
+        "J": 11, "Q": 12, "K": 13, "A": 14
+    }
+    int_rank = rank_dict.get(str_rank)
+    return int_rank
+
+def getSuit(hand):
+    str_suit = hand[-1]
+    
+    # Dictionary seems like the simplest way to
+    # associate strings with int values (and look up
+    # those associations easily)
+    valid_suits = { "H", "S", "C", "D" }
+    if(str_suit in valid_suits):
+        return str_suit
+    else:
+        return None
+
+    
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,99 +1,60 @@
-from poker_scoring_api import (
-    score_poker_hands, card_parser, compare_cards, parse_hand, 
-    get_sorted_hand, has_pair, pair_beats_non_pair, compare_rank_lists,
-    compare_pair_hands)
-
+from poker_scoring_api import score_poker_hands
+from poker_scoring_api import getRank
+from poker_scoring_api import getSuit
 import pytest
 
+def test_basic_interface():
+    result = score_poker_hands("2D","5S")
+    assert isinstance(result, int)
 
-def test_card_parser():
-    assert card_parser("2H") == 2
+# Refactored into the single "parameterized" test below:
 
-def test_compare_cards_less():
-    assert compare_cards("4H", "3C") == 1
+# def test_player1_wins():
+#     assert score_poker_hands("5H","3D") == 1
 
-def test_compare_cards_equal():
-    assert compare_cards("5H", "5S") == 0
+# def test_player2_wins():
+#     assert score_poker_hands("5H","9D") == 2
 
-def test_compare_cards_greater():
-    assert compare_cards("5H", "6S") == 2
+# def test_player1_wins_higher_rank():
+#     assert score_poker_hands("10C","9S") == 1
 
-def test_card_parse_face_cards_jack():
-    assert card_parser("JH") == 11
-    assert card_parser("jH") == 11
+@pytest.mark.parametrize(("hand1", "hand2", "result"),  [
+    ("5H", "3D", 1),
+    ("5H", "9D", 2),
+    ("10C", "9S", 1),
+    ("AS", "KH", 1),
+    ("2C", "JD", 2)
+])
+def test_score_poker_hands(hand1, hand2, result):
+    assert score_poker_hands(hand1, hand2) == result
 
-def test_card_parse_face_cards_queen():
-    assert card_parser("QH") == 12
-    assert card_parser("qH") == 12
+# Reimplemented using parameters below:
 
-def test_card_parse_face_cards_king():
-    assert card_parser("KH") == 13
-    assert card_parser("kH") == 13
+# def test_getRank_func():
+#     assert getRank("KD") == 13
 
-def test_card_parse_face_cards_ace():
-    assert card_parser("AH") == 14
-    assert card_parser("aH") == 14
+@pytest.mark.parametrize(("hand", "rank"), [
+    ("2H", 2),
+    ("10C", 10),
+    ("KD", 13),
+    ("XX", None)
+])
+def test_getRank_func(hand, rank):
+    assert getRank(hand) == rank
 
-def test_card_JS_should_lose_against_QH():
-    assert score_poker_hands("JS", "QH") == 2
+@pytest.mark.parametrize(("hand", "valid"), [
+    ("H", "H"),
+    ("X", None)
+])
+def test_getSuit_func(hand, valid):
+    assert getSuit(hand) == valid
 
-def test_invalid_card_not_string():
+@pytest.mark.parametrize(("hand1", "hand2"), [
+    ("2H", "22H"),
+    ("3F", "10H"),
+    (12, "10S"),
+    ("AS", None)
+])
+def test_invalid_input(hand1,hand2):
     with pytest.raises(ValueError):
-        assert score_poker_hands(0, 0)
-
-def test_invalid_suit():
-    with pytest.raises(ValueError, match = "Invalid suit."):
-        assert score_poker_hands("2Z","2Z")
-
-def test_invalid_rank():
-    with pytest.raises(ValueError, match = "Invalid rank."):
-        assert score_poker_hands("ZH","14H")
-
-def test_invalid_rank():
-    with pytest.raises(ValueError, match = "Invalid rank."):
-        assert score_poker_hands("ZH","10H")
-
-def test_parse_hand():
-    assert parse_hand("3H 2D JC") == ["3H", "2D", "JC"]
-
-def test_parse_single_hand():
-    assert parse_hand("3H") == ["3H"]
-
-def test_multi_card_hand():
-    assert score_poker_hands("3H AD JC", "6S TH 2C") == 1
-
-def test_get_sorted_hand():
-    assert get_sorted_hand(["6S", "TH", "2C"]) == [10, 6, 2]
-
-def test_has_pair():
-    assert has_pair("2H 7C TS") == False
-    assert has_pair("2H 2S TS") == True
-    assert has_pair("2H 2S") == True
-
-def test_pair_beats_non_pair():
-    assert pair_beats_non_pair("2H 2S", "7C TS") == 1
-    assert pair_beats_non_pair("3H 2S", "7C 7S") == 2
-    assert pair_beats_non_pair("2H 6S", "7C TS") == 0
-
-def test_score_poker_hands_pair():
-    assert score_poker_hands("2H 2S", "7C TS") == 1
-
-def test_score_poker_hand_tie():
-    assert score_poker_hands("6D", "6C") == 0
-    assert score_poker_hands("6D 6C", "6H 6S") == 0
-    assert score_poker_hands("6D 5C", "6H 3S") == 1
-
-def test_compare_rank_lists():
-    assert compare_rank_lists([9, 4, 3], [8, 4, 3]) == 1
-    assert compare_rank_lists([9, 4, 3], [9, 4, 2]) == 1
-    assert compare_rank_lists([9, 4, 3], [9, 4, 3]) == 0
-    assert compare_rank_lists([9, 4, 3], [9, 4]) == 1
-
-# Failing because we aren't evaluating the relative rank of pairs
-# (e.g. a pair of 4s should beat a pair of 3s)
-# def test_compare_pairs():
-#      assert score_poker_hands("3C AC TD 3H 4S", "JC 7S 4H QD 4C") == 2
-
-# This function isn't fully implemented yet
-# def test_call_compare_pair_hands():    
-#     assert compare_pair_hands(["3C","AC","TD","4H","4S"], ["JC","7S","3H","QD","3C"]) == 1
+        score_poker_hands(hand1,hand2)
