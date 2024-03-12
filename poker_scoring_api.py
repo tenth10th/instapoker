f13cf9fc2100f78038ee476f018f73c5b72f11b4
David Sturgis <tenth10th@gmail.com>
2024-03-12 01:37:44 +0000

    Misc Cleanup * remove next_time.md from other session! (whoops)
    * Apply Black formatting to poker_scoring_api and test_poker_scoring

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,39 +1,43 @@
 from typing import Tuple, Iterable
 
+
 # TODO: Implement poker scoring api here
-def score_poker_hands(hand1: str, hand2:str) -> int:
+def score_poker_hands(hand1: str, hand2: str) -> int:
     """
     Accept two hands as strings, return an integer
     """
     if hand1 == None or hand2 == None:
-        raise ValueError('Bad input!')
+        raise ValueError("Bad input!")
 
-    split_hand1 = hand1.split(' ')
-    split_hand_2 = hand2.split(' ')
+    split_hand1 = hand1.split(" ")
+    split_hand_2 = hand2.split(" ")
 
     max_hand_1 = max([parse_card(hand)[0] for hand in split_hand1])
     max_hand_2 = max([parse_card(hand)[0] for hand in split_hand_2])
 
     return 1 if max_hand_1 > max_hand_2 else 2
 
+
 def parse_card(card: str) -> Tuple[str, str]:
     if card == None or len(card) < 2:
-        raise ValueError('Bad card!')
+        raise ValueError("Bad card!")
 
     rank = card[0:-1]
     suit = card[-1]
 
     # Mapping of Card ranks to comparable integers
     rank_map = {str(i): i for i in range(2, 11)}
-    rank_map.update({
-        "J": 11,
-        "Q": 12,
-        "K": 13,
-        "A": 14,
-    })
+    rank_map.update(
+        {
+            "J": 11,
+            "Q": 12,
+            "K": 13,
+            "A": 14,
+        }
+    )
 
     try:
-        rank = rank_map[rank] # KeyError if not found
+        rank = rank_map[rank]  # KeyError if not found
     except KeyError:
         raise ValueError("Bad rank!")
 
@@ -44,17 +48,6 @@ def parse_card(card: str) -> Tuple[str, str]:
         raise ValueError("Bad suit!")
 
     return (rank, suit)
-
-
-
-
-
-
-
-
-
-
-
 
 
 """
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,50 +1,65 @@
 from poker_scoring_api import score_poker_hands, parse_card
 import pytest
 
+
 def test_interface():
     result = score_poker_hands("3D 2H KS 5D 9C", "5H JD 10H AC JS")
     assert isinstance(result, int)
 
-@pytest.mark.parametrize(('hand1', 'hand2', 'expected'), [
-    ('2D', '3H', 2),
-    ('2D 3H JD 10C KS', '2H 3D 4C 5S 6H', 1),
-    ('3H', '2D', 1),
-    ('JD', '10D', 1),
-    ('10C', '2H', 1),
-])
+
+@pytest.mark.parametrize(
+    ("hand1", "hand2", "expected"),
+    [
+        ("2D", "3H", 2),
+        ("2D 3H JD 10C KS", "2H 3D 4C 5S 6H", 1),
+        ("3H", "2D", 1),
+        ("JD", "10D", 1),
+        ("10C", "2H", 1),
+    ],
+)
 def test_score_poker_hands(hand1, hand2, expected):
     result = score_poker_hands(hand1, hand2)
     assert result == expected
 
-@pytest.mark.parametrize(('hand1', 'hand2'), [
-    (None, '3H'),
-    ('2D 3H JD 10C KS', None),
-])
+
+@pytest.mark.parametrize(
+    ("hand1", "hand2"),
+    [
+        (None, "3H"),
+        ("2D 3H JD 10C KS", None),
+    ],
+)
 def test_bad_input_score_poker_hands(hand1, hand2):
     with pytest.raises(ValueError) as ve:
         # in this block, raising ValueError
         # is expected!
         score_poker_hands(hand1, hand2)
-    assert 'input' in str(ve)
+    assert "input" in str(ve)
 
 
-
-@pytest.mark.parametrize(('card', 'expected'), [
-    ("2S", (2, "S")),
-    ("10H", (10, "H")),
-    ("JD", (11, "D")),
-])
+@pytest.mark.parametrize(
+    ("card", "expected"),
+    [
+        ("2S", (2, "S")),
+        ("10H", (10, "H")),
+        ("JD", (11, "D")),
+    ],
+)
 def test_parse_card(card, expected):
     result = parse_card(card)
     assert result == expected
 
-@pytest.mark.parametrize(('card', 'expected_text'), [
-    ('KF', 'suit'),
-    ('FH', 'rank'),
-    ('FF', 'rank'),
-    (None, 'card'),
-    ('', 'card'),
-])
+
+@pytest.mark.parametrize(
+    ("card", "expected_text"),
+    [
+        ("KF", "suit"),
+        ("FH", "rank"),
+        ("FF", "rank"),
+        (None, "card"),
+        ("", "card"),
+    ],
+)
 def test_invalid_rank(card, expected_text):
     with pytest.raises(ValueError) as ve:
         # in this block, raising ValueError
