dd55c0d91b1f4145ce6cb2e194c73047395da5e4
David Sturgis <tenth10th@gmail.com>
2024-04-09 01:32:52 +0000

    WIP for the April 2024 session

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,3 +1,76 @@
-# TODO: Implement poker scoring api here
-# def score_poker_hands():
-#    pass
+from typing import Optional, Tuple
+from enum import IntEnum
+
+
+def score_poker_hands(hand1_str: str, hand2_str: str) -> int:
+    if not isinstance(hand1_str, str) or not isinstance(hand1_str, str):
+        raise ValueError("Please check hand types or be fired")
+    hand1 = Hand(hand1_str)
+    hand2 = Hand(hand2_str)
+    return 1 if hand1.value() > hand2.value() else 2
+
+
+class Card:
+    def __init__(self, card_str: str):
+        if len(card_str) < 2:
+            raise ValueError("Cards must be at least two characters")
+        self.suit = card_str[-1]
+        if self.suit not in "HDCS":
+            raise ValueError("That is not a valid suit")
+        self.rank_str = card_str[0:-1]
+        self.int_rank()
+    # TODO: Add accessors? validators?
+
+    rank_values = {
+        "T": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    }
+
+    def int_rank(self):
+        self.rank = self.rank_values.get(
+            self.rank_str,
+        )
+        if self.rank is None:
+            try:
+                self.rank = int(self.rank_str)
+            except ValueError:
+                raise ValueError("That is not a valid rank")
+            if self.rank < 2 or self.rank > 9:
+                raise ValueError("The card rank must be between 2 and 9")
+
+
+class HandType(IntEnum):
+    HIGHEST_CARD = 0
+    PAIR = 1
+
+
+class Hand:
+    def __init__(self, hand_str: str):
+        self.cards = list(map(Card, hand_str.split(" ")))
+
+    def highest_card(self):
+        print("self.cards", self.cards)
+        # Eventually, this could get us the "kicker" (for ties)
+        return max(map(lambda x: x.rank, self.cards))
+
+    def has_pair(self) -> Optional[int]:
+        rank_counts: dict[int, int] = dict()
+        for card in self.cards:
+            rank_counts[card.rank] = rank_counts.get(card.rank, 0) + 1
+
+        best_pair = None
+        for rank, count in rank_counts.items():
+            if count >= 2 and (best_pair is None or rank > best_pair):
+                best_pair = rank
+        return best_pair
+
+    def value(self) -> Tuple[int, int]:
+        pair_value = self.has_pair()
+        high_card = self.highest_card()
+        hand_type = HandType.PAIR if pair_value else HandType.HIGHEST_CARD
+        hand_value = pair_value or high_card
+        # Need to re-evaluate this as more Hands are added!
+        return hand_type, hand_value
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,5 +1,7 @@
 # TODO: Write some tests for score_poker_hands...
 # from poker_scoring_api import score_poker_hands
+from poker_scoring_api import Card, Hand, score_poker_hands
+import pytest
 
 
 def test_example():
@@ -7,3 +9,79 @@ def test_example():
     # a function whose name start with "test"
     # (that ideally asserts some stuff...)
     assert True
+
+
+@pytest.mark.parametrize("card, rank, suit", [
+    ("2H", 2, "H"),
+    ("JD", 11, "D"),
+    ("TS", 10, "S")
+])
+def test_card_parsing(card, rank, suit):
+    c = Card(card)
+    assert c.rank == rank
+    assert c.suit == suit
+
+
+def test_hand():
+    h = Hand("2H")
+    assert h.highest_card() == 2
+
+
+@pytest.mark.parametrize("h1, h2, expected", [
+    ("TH", "2D", 1),
+    ("2D", "TH", 2),
+])
+def test_score_poker_hands(h1, h2, expected):
+    result = score_poker_hands(h1, h2)
+    assert result == expected
+
+
+@pytest.mark.parametrize("h1, h2, expected, except_message", [
+    ("GH", "2D", ValueError, "That is not a valid rank"),
+    ("99H", "2D", ValueError, "The card rank must be between 2 and 9"),
+    ("2F", "6F", ValueError, "That is not a valid suit"),
+    ("10H", "6D", ValueError, "The card rank must be between 2 and 9"),
+    (None, None, ValueError, "Please check hand types or be fired"),
+    ("", "", ValueError, "Cards must be at least two characters"),
+    (1, False, ValueError, ""),
+])
+def test_score_poker_hands_error(h1, h2, expected, except_message):
+    with pytest.raises(expected) as ex:
+        _ = score_poker_hands(h1, h2)
+    if except_message:
+        assert except_message in str(ex)
+
+
+@pytest.mark.parametrize("h1, h2, expected", [
+    ("2H 3D", "4C 6S", 2),
+    ("2H 8D", "4C 6S", 1),
+])
+def test_score_poker_hands_multiple_cards(h1, h2, expected):
+    result = score_poker_hands(h1, h2)
+    assert result == expected
+
+
+@pytest.mark.parametrize(("hand_str", "best_pair"), [
+    ("2H 3D 2C", 2),
+    ("2H 3D 2C 3S", 3),
+    ("2H 3D", None),
+    ("2H 3C 2D 2S", 2),
+])
+def test_hand_has_pair(hand_str, best_pair):
+    h = Hand(hand_str)
+    assert h.has_pair() == best_pair
+
+
+
+@pytest.mark.parametrize("h1, h2, expected", [
+    ("2H 3D 2S", "4C 6S", 1),
+    ("2H 8D", "4C 6S 4D", 2),
+])
+def test_score_poker_hands_pairs(h1, h2, expected):
+    result = score_poker_hands(h1, h2)
+    assert result == expected
+
+
+def test_hand_highest_card():
+    h = Hand("2H 3D 2S")
+    print(h.highest_card())
