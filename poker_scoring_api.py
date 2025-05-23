39f16289d3cc1edda039b317583fcd910be06336
David Sturgis <tenth10th@gmail.com>
2025-05-23 01:19:25 +0000

    BSC 2025-05-22 session WIP

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,3 +1,23 @@
 # TODO: Implement poker scoring api here
-# def score_poker_hands():
-#    pass
+def score_poker_hands(hand1: str, hand2: str) -> int:
+
+    invalids = ("1h", "2F")
+
+    if not hand1:
+        raise ValueError()
+    elif type(hand1) is int:
+        raise ValueError()
+    elif hand1 in invalids:
+        raise ValueError()
+
+    rank1 = hand1[:-1]
+    rank2 = hand2[:-1]
+    if rank_value(rank1) > rank_value(rank2):
+        return 1
+    else:
+        return 2
+
+
+def rank_value(rank: str) -> int:
+    values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    return values.index(rank) + 1
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,9 +1,39 @@
-# TODO: Write some tests for score_poker_hands...
-# from poker_scoring_api import score_poker_hands
+import pytest
+
+from poker_scoring_api import score_poker_hands, rank_value
 
 
-def test_example():
-    # Example of a PyTest test case:
-    # a function whose name start with "test"
-    # (that ideally asserts some stuff...)
-    assert True
+def test_api():
+    assert isinstance(score_poker_hands("2H", "5D"), int)
+
+
+@pytest.mark.parametrize(
+    ("hand1", "hand2", "result"),
+    (
+        ("2H", "5D", 2),
+        ("JH", "AD", 2),
+        ("AH", "KD", 1),
+        ("QH", "JD", 1),
+        ("5H", "2D", 1),
+        ("AH", "JD", 1),
+        ("KH", "AD", 2),
+        ("JH", "QD", 2),
+        ("KH", "QD", 1),
+        ("10H", "9D", 1),
+        ("9H", "10D", 2),
+    ),
+)
+def test_single_card_hands(hand1, hand2, result):
+    assert score_poker_hands(hand1, hand2) == result
+
+
+@pytest.mark.parametrize(("rank", "result"), (("5", 5), ("7", 7), ("J", 11), ("K", 13)))
+def test_the_rank_value(rank, result):
+    assert rank_value(rank) == result
+
+
+@pytest.mark.parametrize(("hand"), ("", "1h", 1, "2F", {}, []))
+def test_bad_inputs(hand):
+    with pytest.raises(ValueError):
+        score_poker_hands("9H", hand)
+        score_poker_hands(hand, "9H")
