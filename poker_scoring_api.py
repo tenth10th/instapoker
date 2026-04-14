187d30e564e370c375cce161a5d89fc8a26f4068
David Sturgis <tenth10th@gmail.com>
2026-04-14 01:39:09 +0000

    Work in progress from BSC 2026-04-13 Session

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,3 +1,61 @@
 # TODO: Implement poker scoring api here
-# def score_poker_hands():
-#    pass
+
+VALID_RANKS: list[str] = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
+# FIXME: Is there an order for suits? (are suits ordered?)
+VALID_SUITS: list[str] = ['H', 'D', 'C', 'S']
+
+MAX_CARD_PER_HAND = 5  # Probably
+
+def score_poker_hands(hand1: str | None, hand2: str | None) -> int:
+    card_tuples1 = _parse_hand(hand1)
+    score1 = _score_cards(card_tuples1)
+
+    card_tuples2 = _parse_hand(hand2)
+    score2 = _score_cards(card_tuples2)
+
+    return 1 if score1 > score2 else 2
+
+def _rank(card: str) -> int:
+    """
+    Return an integer representation of the rank of a valid card
+    raises ValueError on invalid ranks (not listed in valid_ranks, above)
+    """
+    rank_str = card[0:-1]
+    return VALID_RANKS.index(rank_str)
+
+def _suit(card: str) -> int:
+    """
+    Return an integer representation of the suit of a valid card
+    (FIXME: Are suits actually ordered or comparable?!)
+    raises ValueError on invalid suits (not listed in valid_suits, above)
+    """
+    suit_str = card[-1]
+    return VALID_SUITS.index(suit_str)
+
+def _parse_card(card: str) -> tuple[int, int]:
+    """
+    Parse a card into a tuple of (rank, suit) integers
+    raises a ValueError in the event of invalid ranks or suits (see above)
+    """
+    if not card:
+        raise ValueError(f"Can't parse card: {card}")
+    rank = _rank(card)
+    suit = _suit(card)
+    return rank, suit
+
+def _parse_hand(hand: str | None) -> list[tuple[int, int]]:
+    if not hand or not hand.strip():
+        raise ValueError(f"Can't parse hand: \"{hand}\"")
+    card_strs = hand.split(" ")
+    cards = [_parse_card(c) for c in card_strs]
+    return cards
+
+def _score_cards(card_tuples: list[tuple[int, int]]) -> int:
+    ranks = [rank for rank, _ in card_tuples]
+    return max(ranks)
+
+"""
+TODO:
+highest_card(hand) function?
+"""
--- test_poker_scoring.py
+++ test_poker_scoring.py
@@ -1,9 +1,50 @@
 # TODO: Write some tests for score_poker_hands...
-# from poker_scoring_api import score_poker_hands
+
+from poker_scoring_api import score_poker_hands
+import pytest
+
+def test_function_signature():
+    """
+    score_poker_hands accepts two hands as strings
+    (and returns an integer, 1 or 2)
+    """
+    result = score_poker_hands("2H", "5D")
+    assert isinstance(result, int)
+    assert result in (1, 2)
 
 
-def test_example():
-    # Example of a PyTest test case:
-    # a function whose name start with "test"
-    # (that ideally asserts some stuff...)
-    assert True
+def test_simple_comparison():
+    assert score_poker_hands("3H", "2D") == 1
+    assert score_poker_hands("2H", "3D") == 2
+
+
+def test_ten_vs_two():
+    assert score_poker_hands("TH", "2D") == 1
+    assert score_poker_hands("2D", "TH") == 2
+
+
+def test_j_greater_than_10():
+    assert score_poker_hands("JD", "TH") == 1
+    assert score_poker_hands("TH", "JD") == 2
+
+
+def test_invalid_rank():
+    with pytest.raises(ValueError):
+        score_poker_hands("99H", "1D")
+
+
+def test_invalid_suit():
+    with pytest.raises(ValueError):
+        score_poker_hands("2F", "9X")
+
+
+def test_none_card():
+    with pytest.raises(ValueError):
+        score_poker_hands(None, None)
+
+def test_multi_card_hands_allowed():
+    score_poker_hands("2H 4D 7C 2C 3S", "2D KS 5D 9C JH")
+
+def test_highest_card_wins():
+    assert score_poker_hands("2H 4D 7C 2C 3S", "2D KS 5D 9C JH") == 2
+    assert score_poker_hands("2D KS 5D 9C JH", "2H 4D 7C 2C 3S") == 1
